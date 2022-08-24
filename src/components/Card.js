75385e3a6b240b9b2192b49faccc1214e012cb03
AhmedElatar <ahmedmohamed00775@gmail.com>
2022-08-24 11:43:33 +0200

    established cards js and css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "./Cstyle.module.css";
+import StarIcon from "@material-ui/icons/Star";
+import StarHalfIcon from "@material-ui/icons/StarHalf"
 
 
 function CourseCard(props) {
-    let c = props.c;
-    return (<div>
 
-    </div>)
+    return (<div className="Card">
+        <img className="courseImg" src={props.ImgSrc} alt="courseImg"></img>
+        <h3>{props.courseTitle}</h3>
+        <p>{props.instructor}</p>
+        <div className="ratingDiv">
+            <span className="rating">{props.rating}</span>
+            <span className="stars">
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarHalfIcon />
+            </span>
+            <span className="noOfStudents">{props.noOfStudents}</span>
+        </div>
+        <h3 className="price">{props.price}</h3>
+        <div className="bestsellerBadge">Bestseller</div>
+    </div>);
 }
 export default CourseCard;
